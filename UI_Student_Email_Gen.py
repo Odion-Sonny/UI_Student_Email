8513e41963b07cf53b5a3fb562695c9bd68cf813
Odion Sonny-Egbeahie <99491084+Odion-Sonny@users.noreply.github.com>
2022-09-20 10:48:47 +0100

    Update UI_Student_Email_Gen.py

--- UI_Student_Email_Gen.py
+++ UI_Student_Email_Gen.py
@@ -1,18 +1,18 @@
 def email_gen(firstname, lastname, matric_no):
-    firstname=firstname.lower()
-    lastname=lastname.lower()
-    domain='@stu.ui.edu.ng'
+    firstname = firstname.lower()
+    lastname = lastname.lower()
+    domain = '@stu.ui.edu.ng'
 
-    matric_no_list=[matric_no[i] for i in range(3,6)]
+    matric_no_list = matric_no[3:]
     
-    matric_no_string=''.join(matric_no_list)
+    matric_no_string =''.join(matric_no_list)
     
-    email=''.join((firstname[0],lastname,matric_no_string,domain))
+    email = ''.join((firstname[0],lastname,matric_no_string,domain))
     return email
 def main():
-    firstname=input('Please Enter Your First Name: ')
-    lastname=input('Please Enter Your Last Name: ')
-    matric_no=input('Please Enter Your Matriculation Number: ')
+    firstname = input('Please Enter Your First Name: ')
+    lastname = input('Please Enter Your Last Name: ')
+    matric_no = input('Please Enter Your Matriculation Number: ')
     print(f'Your Student Email Is: {email_gen(firstname, lastname, matric_no)}')    
 
 main()
