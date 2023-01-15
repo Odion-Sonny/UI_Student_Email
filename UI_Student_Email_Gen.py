2c60a0842b348ff8dac44cbfb27edb77577b27d9
Odion Sonny-Egbeahie <99491084+Odion-Sonny@users.noreply.github.com>
2023-01-15 02:37:05 +0100

    Update UI_Student_Email_Gen.py

--- UI_Student_Email_Gen.py
+++ UI_Student_Email_Gen.py
@@ -2,13 +2,17 @@ def email_gen(firstname, lastname, matric_no):
     firstname = firstname.lower()
     lastname = lastname.lower()
     domain = '@stu.ui.edu.ng'
-
+    
+    # take last three digits of matric number
     matric_no_list = matric_no[3:]
     
     matric_no_string =''.join(matric_no_list)
     
+    # add first letter of firstname + lastname + last three digits of matric number + domain name
     email = ''.join((firstname[0],lastname,matric_no_string,domain))
+    
     return email
+
 def main():
     firstname = input('Please Enter Your First Name: ')
     lastname = input('Please Enter Your Last Name: ')
